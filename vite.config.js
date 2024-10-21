e1bac216e9c0575e0b0257e4337e8252100ae91a
Anushka <kishoranushka96@gmail.com>
2024-10-21 09:59:39 +0530

    chnges made in vite config file and index.html file

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/pinakassignment/", // Use your repo name here
+  base: "/pinakassignment/", 
   root: "src",
   build: {
     outDir: "../dist",
