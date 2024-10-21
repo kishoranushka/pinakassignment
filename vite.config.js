5bbc5c53dba04ec36f2e632f04577479c9657448
Anushka <kishoranushka96@gmail.com>
2024-10-21 10:10:52 +0530

    add redirects file in public folder

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/pinakassignment/", 
+  base: "/pinakassignment/",
   root: "src",
+  publicDir: "../public",
   build: {
     outDir: "../dist",
   },
